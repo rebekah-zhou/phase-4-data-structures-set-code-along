8fc0885c8ff2a6cab5cf1097416200f2339336d9
Rebekah Zhou <rebekahzhou@gmail.com>
2022-03-14 16:27:46 -0700

    commit tree 1413368573e59e669a33dbbdf25e8b49637b13e6

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,29 @@
 # your code here
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(num)
+        @hash.has_key?(num)
+    end
+
+    def add(num)
+        if !include?(num)
+            @hash[num] = true
+        end
+        self
+    end
+
+    def delete(num)
+        @hash.delete(num)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+end
